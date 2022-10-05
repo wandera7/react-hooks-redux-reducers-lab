db1ac3c59360e1977feda9811415301c1f8333ae
wandera7 <julius.wandera@student.moringaschool.com>
2022-10-05 06:56:31 +0300

    Done.

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,13 @@
 export function manageFriends(state, action) {
   // your code here
+  switch(action.type){
+    case 'friends/add':
+      return {...state, friends: [...state.friends, action.payload]}
+    case 'friends/remove':
+    return {...state, friends:state.friends.filter((friend)=>{
+        return friend.id != action.payload
+      })}
+    default:
+      return state
+  }
 }
